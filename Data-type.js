f4f83470ca9ca03bf6da8579a3eac65978859a72
aswinsagar12 <aswinsagar12@gmail.com>
2021-11-27 23:09:32 +0530

    DataType

---
+++ Data-type.js
@@ -0,0 +1,30 @@
+//number
+
+var number = 10;
+console.log(number);
+console.log(typeof number);
+
+
+
+//string
+var name = "hey";
+console.log(typeof name);
+console.log(name);
+
+
+//array
+var arr = ["one", "two"];
+console.log(arr);
+console.log(typeof arr);
+
+
+//undefined
+var x;
+console.log(x);
+console.log(typeof x);
+
+//null
+
+var xyz = null;
+console.log(xyz);
+console.log(typeof xyz);
