efed16ba81965df2e61c98fa2b65b2773d1a0fe8
aswinsagar12 <aswinsagar12@gmail.com>
2021-11-27 23:09:45 +0530

    Function

---
+++ student.js
@@ -0,0 +1,40 @@
+function Student(name, Class, age) {
+    this.name = name;
+    this.classes = Class;
+    this.age = age;
+
+}
+
+Student.prototype.addStudent = function() {
+    console.log("Student Name " + this.name);
+
+}
+
+Student.prototype.update = function(value) {
+    console.log("Student age details updating " + this.age);
+
+}
+Student.prototype.get = function() {
+
+    console.log("Getting data ");
+}
+
+
+
+Student.prototype.print = function(name) {
+
+        console.log("printing " + this.name + "'s Data ");
+    }
+    //final out
+console.log(Student.prototype.addStudent);
+var std = new Student("Aswin", "12th", 20);
+console.log(std);
+
+std.print("Datas");
+std.update("Aswin Sagar");
+std.get();
+Student.prototype.delete = function(value) {
+
+    console.log("Deleting " + this.name + " datas");
+}
+std.delete("name");
